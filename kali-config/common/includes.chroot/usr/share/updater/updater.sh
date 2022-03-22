73d63c01f79345841631d519be92b5f0f4c09a0f
Tom <tom@humandecoded.io>
2022-03-22 16:39:06 -0400

    fixed updater

--- kali-config/common/includes.chroot/usr/share/updater/updater.sh
+++ kali-config/common/includes.chroot/usr/share/updater/updater.sh
@@ -2,13 +2,13 @@
 
 cd $HOME
 
-wget -O updater-current.sh https://raw.githubusercontent.com/tracelabs/tlosint-live/master/updater-current.sh
+wget -O updater-current.sh https://raw.githubusercontent.com/tracelabs/Trace-Labs-VM-M1-Mac-Build/main/updater-current.sh
 
 chmod +x updater-current.sh
 
 sudo $HOME/updater-current.sh
 
-wget -O bookmarks.html https://raw.githubusercontent.com/tracelabs/tlosint-live/master/bookmarks.html
+wget -O bookmarks.html https://raw.githubusercontent.com/tracelabs/Trace-Labs-VM-M1-Mac-Build/main/bookmarks.html
 
 rm -f updater-current.sh
 
